c9b2a3f3c0d8c97c0394887da4c9fab25ed68746
Anthony Ricardo <anthony.ricardo@mikroskil.ac.id>
2017-12-02 12:01:22 +0700

    initial setup

---
+++ Objects/Game.js
@@ -0,0 +1,6 @@
+function Game() {
+  this.time = 0;
+  this.score = 0;
+  this.max_inventory = 5;
+  this.life = 100;
+}
---
+++ Objects/Word.js
@@ -0,0 +1,7 @@
+let listOfWord = ["Argentina", "Brazil", "Cat", "Devil", "Eagle", "Friendster", "Gilgamesh", "Hikikomori"];
+
+function Word(){
+  this.value = 0;
+  this.score = 0;
+  this.speed = 100;
+}
